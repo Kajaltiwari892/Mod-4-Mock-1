3216bdd95c7c19444ef2e4860f357a6acd7cd5c7
Kajaltiwari892 <kanjal55567@gmail.com>
2025-02-22 16:23:39 +0530

    Ecommerce with routing &thunk

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import { Provider } from "@/components/ui/provider";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider , extendTheme } from "@chakra-ui/react";
+
+ const theme = extendTheme({});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ChakraProvider>
-      <Provider>
+    <Provider>
+      <ChakraProvider>
         <App />
-      </Provider>
-    </ChakraProvider>
+      </ChakraProvider>
+    </Provider>
   </StrictMode>
 );
